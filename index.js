01ffc654063f01de1137721a552417b325470594
smily000 <91569111+smily000@users.noreply.github.com>
2022-11-28 14:21:04 -0600

    sfd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const mongoose = require("mongoose");
 //lol2
 
-const mongooseUri = "mongodb+srv://signupsheet:lol2@cluster0.liijka6.mongodb.net/lol" ;
+const mongooseUri = "mongodb+srv://d:dan@cluster0.6un8qsu.mongodb.net/man" ;
 mongoose.connect(mongooseUri, {useNewUrlParser: true}, {useUnifiedTopology: true})
 const movieSchema = {
 	title: String,
