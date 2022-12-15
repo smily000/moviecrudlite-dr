ca12a1cdaf09bc966dbe590a3ae7a2e52f8e0449
smily000 <91569111+smily000@users.noreply.github.com>
2022-12-14 22:07:20 -0600

    lol

--- client/map.js
+++ client/map.js
@@ -4,10 +4,13 @@ var gMap;
 function initMap(){
     gMap = new google.maps.Map(document.getElementById('myMapID'),{
         center: {lat: 41.878,lng:10}, zoom:3});
+
+    var marker = new google.maps.Marker({position:{lat:41.6048,lng:-88.0796},map:gMap});
+    var infoWindow = new google.maps.infoWindow({content:'lewis unversity'});
+    marker.addListener('click',function(){
+        infoWindow.open(gMap,marker);
+    });
 }
-
-
-
 
 function initApplication(){
     console.log('map mania Lite- starting');
