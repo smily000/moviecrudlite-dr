cc9a87461b7d24f2fdb01c45dabfe4c489dbf196
smily000 <91569111+smily000@users.noreply.github.com>
2022-12-14 22:44:48 -0600

    lol

--- client/map.js
+++ client/map.js
@@ -1,14 +1,16 @@
+const { ServerDescriptionChangedEvent } = require("mongodb");
 
 var gMap;
 
 function initMap(){
     gMap = new google.maps.Map(document.getElementById('myMapID'),{
-        center: {lat: 41.878,lng:10}, zoom:3});
+        center: {lat: 37.0902,lng:-95.7129}, zoom:3});
 
     var marker = new google.maps.Marker({position:{lat:41.6048,lng:-88.0796},map:gMap});
     var infoWindow = new google.maps.infoWindow({content:'lewis unversity'});
     marker.addListener('click',function(){
         infoWindow.open(gMap,marker);
+        
     });
 }
 
