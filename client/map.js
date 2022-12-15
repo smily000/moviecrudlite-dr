d02de05f3f22debab8a33a576605b6f7eb0fb576
smily000 <91569111+smily000@users.noreply.github.com>
2022-12-14 20:56:32 -0600

    LOL

---
+++ client/map.js
@@ -0,0 +1,14 @@
+
+var gMap;
+
+function initMap(){
+    gMap = new google.maps.Map(document.getElementById('myMapID'),{
+        center: {lat: 41.878,lng:10}, zoom:3});
+}
+
+
+
+
+function initApplication(){
+    console.log('map mania Lite- starting');
+}
